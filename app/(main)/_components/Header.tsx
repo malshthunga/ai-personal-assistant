26cb393bf0a921cdb3e40b91f50627a90f53b9d7
Nethmi <nethmimal21@gmail.com>
2025-07-30 10:09:07 +0930

    updating header

--- app/(main)/_components/Header.tsx
+++ app/(main)/_components/Header.tsx
@@ -1,33 +1,35 @@
 'use client'
-import React, { useContext } from 'react'
-import Image from 'next/image'
-import { AuthContext } from '@/context/AuthContext'
+import React, { useContext } from 'react';
+import Image from 'next/image';
+import { AuthContext } from '@/context/AuthContext';
 
 function Header() {
-  const {user}= useContext(AuthContext);
-  return user && (
-        <div
-      style={{
-        backgroundColor: 'hotpink',
-        padding: '16px',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}
-    ></div>
-    // <div className='p-3 shadow-sm flex justify-between items-center px-14'> 
-    //   <Image src={'/logo.svg'} alt='logo'
-    //     width={40}
-    //     height={40}/>
+  const { user } = useContext(AuthContext);
 
-      // {user?.picture && <Image src={user?.picture} alt='logo'
-      //   width={40}
-      //   height={40}
-      //   className='rounded-full'
+  return (
+    <div className='p-3 shadow-sm flex justify-between items-center px-14 bg-white'>
+      {/* Logo */}
+      <Image
+        src={'/logo.svg'}
+        alt='logo'
+        width={40}
+        height={40}
+      />
 
-    //   // />}
-    // </div>
-  )
+      {/* User Avatar */}
+      {user?.picture ? (
+        <Image
+          src={user.picture}
+          alt='User'
+          width={40}
+          height={40}
+          className='rounded-full'
+        />
+      ) : (
+        <p className='text-gray-600'>Guest</p>
+      )}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
